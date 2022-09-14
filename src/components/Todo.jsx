7bebd15213e03692565db7c5d657f0d3da928e76
KamalBamma <kamal.bamma21@gmail.com>
2022-09-14 10:57:45 +0545

    add

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Todo.css";
 
 const Todo = () => {
   const [inputData, setInputData] = useState("");
-  const [todos, setTodos] = useState([
-    "Walk for fresh.",
-    "Rest a while.",
-    "Feed a dog.",
-    "Watch a movie.",
-  ]);
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("lists")) || []
+  );
 
   console.log(inputData);
+
+  //use effect to set the todos array to local storage
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(todos));
+  }, [todos]);
+
+  // use effect to fetch data from local storage
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("lists"));
+    if (data) {
+      setTodos(data);
+    }
+  }, []);
 
   // Create: Add new items(tasks) to the existing todo array.
   // Arrow function can also be used
@@ -52,7 +62,7 @@ const Todo = () => {
           className="input"
           placeholder="Add items"
           value={inputData}
-          onChange={(e) => setInputData(e.target.value)}
+          onChange={e => setInputData(e.target.value)}
         />
         <input
           type="button"
@@ -66,21 +76,21 @@ const Todo = () => {
       <div className="todo-lists">
         <h3>Task Lists</h3>
         <ul>
-          {todos.map((todo) => (
-            <li>
-              {todo}
-              <span
-                className="delete"
-                onClick={() => deleteTodo(todos.indexOf(todo))}
-              >
-                X
-              </span>
-              <span
+          {todos.map((todo, index) => (
+            <li key={index}>
+              <span>{todo}</span>
+              <input
+                type="button"
                 className="update"
-                onClick={() => updateTodo(todos.indexOf(todo))}
-              >
-                U
-              </span>
+                value="Update"
+                onClick={() => updateTodo(index)}
+              />
+              <input
+                type="button"
+                className="delete"
+                value="Delete"
+                onClick={() => deleteTodo(index)}
+              />
             </li>
           ))}
         </ul>
